c42664c30195e764b2208dd79fbc3eaa87088a50
Joseph <sepej@oregonstate.edu>
2024-05-20 21:00:07 -0700

    added some comments

--- random_image_from_directory.py
+++ random_image_from_directory.py
@@ -1,6 +1,19 @@
+import os
+import random
+from flask import Flask, send_file
 
-def main():
-    print("microservice started")
+app = Flask(__name__)
+
+image_directory = "images"
+
+@app.route("/random_image")
+def random_image():
+    # get a list of images in the directory
+    images = os.listdir(image_directory)
+    # select an image from random
+    random_image = random.choice(images)
+    # send file
+    return send_file(os.path.join(image_directory, random_image))
 
 if __name__ == '__main__':
-    main()
+    app.run(debug=True)
