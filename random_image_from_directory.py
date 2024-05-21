c961aa02f66effd9262295f6dd43b4a8fb2cfa67
Joseph <sepej@oregonstate.edu>
2024-05-20 22:32:17 -0700

    added example request with HTML/JS

--- random_image_from_directory.py
+++ random_image_from_directory.py
@@ -1,11 +1,17 @@
 import os
 import random
-from flask import Flask, send_file
+from flask import Flask, send_file, render_template
 
 app = Flask(__name__)
 
 CONFIG_PATH = "config.txt"
 
+# home routes to an example request using HTML and JS
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+# the route for random image
 @app.route("/random_image")
 def random_image():
     image_directory = ""
